aafc729b3bc8861cfef1225640e406ec058fc648
Xiaoli Shen <alabebop@gmail.com>
2023-06-26 10:56:59 +0900

    Add new mnmcount code

--- chapter2/py/src/mnmcount.py
+++ chapter2/py/src/mnmcount.py
@@ -1,45 +1,52 @@
-from __future__ import print_function
-
 import sys
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import count
 
 if __name__ == "__main__":
+    # Safeguard usage
     if len(sys.argv) != 2:
         print("Usage: mnmcount <file>", file=sys.stderr)
         sys.exit(-1)
 
-    spark = (SparkSession
-        .builder
-        .appName("PythonMnMCount")
-        .getOrCreate())
-    # get the M&M data set file name
+    # Build a SparkSession (Create one if it doesn't exist)
+    spark = SparkSession.builder.appName("PythonMnMCount").getOrCreate()
+
+    # Load data into dataframe
+    # from the file specified as command-line argument
     mnm_file = sys.argv[1]
-    # read the file into a Spark DataFrame
-    mnm_df = (spark.read.format("csv")
+    mnm_df = (
+        spark.read.format("csv")
         .option("header", "true")
         .option("inferSchema", "true")
-        .load(mnm_file))
-    mnm_df.show(n=5, truncate=False)
+        .load(mnm_file)
+    )
 
-    # aggregate count of all colors and groupBy state and color
-    # orderBy descending order
-    count_mnm_df = (mnm_df.select("State", "Color", "Count")
-                    .groupBy("State", "Color")
-                    .sum("Count")
-                    .orderBy("sum(Count)", ascending=False))
-
-    # show all the resulting aggregation for all the dates and colors
+    # Calculate the count for all states
+    # 1. select "State", "Color", "Count"
+    # 2. Group each state and its M&M color count
+    # 3. Aggregate counts of all colors and alias the count "Total"
+    # 4. orderBy descending
+    count_mnm_df = (
+        mnm_df.select("State", "Color", "Count")
+        .groupBy("State", "Color")
+        .agg(count("Count").alias("Total"))
+        .orderBy("Total", ascending=False)
+    )
+    # show the result aggregations for all states and colors
     count_mnm_df.show(n=60, truncate=False)
-    print("Total Rows = %d" % (count_mnm_df.count()))
+    print(f"Total Rows = {count_mnm_df.count()}")
 
-    # find the aggregate count for California by filtering
-    ca_count_mnm_df = (mnm_df.select("*")
-                       .where(mnm_df.State == 'CA')
-                       .groupBy("State", "Color")
-                       .sum("Count")
-                       .orderBy("sum(Count)", ascending=False))
-
-    # show the resulting aggregation for California
+    # Calculate the count just for state California
+    ca_count_mnm_df = (
+        mnm_df.select("State", "Color", "Count")
+        .where(mnm_df.State == "CA")
+        .groupBy("State", "Color")
+        .agg(count("Count").alias("Total"))
+        .orderBy("Total", ascending=False)
+    )
+    # Show California result
     ca_count_mnm_df.show(n=10, truncate=False)
 
+    # Stop the SparkSession
+    spark.stop()
